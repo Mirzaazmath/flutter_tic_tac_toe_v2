eeb13b45fa10657fd0b57134739b577d67c2c38e
Mirzaazmath <azmath9603399270@gmail.com>
2025-05-07 17:47:24 +0530

    added requied files for code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tic_tac_toe/screens/splash_screen.dart';
 void main(){
   runApp(MyApp());
 }
@@ -7,6 +8,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: SplashScreen(),
+    );
   }
 }
