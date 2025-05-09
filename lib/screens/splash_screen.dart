a8894d151d93b6a727ad7533c574a60ac22d8699
Mirzaazmath <azmath9603399270@gmail.com>
2025-05-09 11:44:39 +0530

    added navigation in splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tic_tac_toe/core/assets.dart';
+import 'package:flutter_tic_tac_toe/screens/home_screen.dart';
+import 'package:flutter_tic_tac_toe/screens/user_name_screen.dart';
 import 'package:flutter_tic_tac_toe/widgets/background_widget.dart';
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,6 +11,19 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3),(){
+      bool isNewUser = true;
+      if(isNewUser){
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>UserNameScreen()));
+      }else {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>HomeScreen()));
+      }
+
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return BackgroundWidget(
