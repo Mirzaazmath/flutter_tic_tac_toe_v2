464cb6c21e1156c1c6a2cb67e39eb3553794c457
Mirzaazmath <azmath9603399270@gmail.com>
2025-05-09 11:59:11 +0530

    added navigation in scan player screen

--- lib/screens/scan_player_screen.dart
+++ lib/screens/scan_player_screen.dart
@@ -1,21 +1,33 @@
 import 'package:device_scan_animation/device_scan_animation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tic_tac_toe/screens/tic_tac_toe_screen.dart';
 
 import '../core/constants/enums.dart';
 import '../widgets/background_widget.dart';
 import '../widgets/custom_button.dart';
 import '../widgets/custom_chip_widget.dart';
 
-class ScanPlayerScreen extends StatelessWidget {
+class ScanPlayerScreen extends StatefulWidget {
   const ScanPlayerScreen({super.key});
 
+  @override
+  State<ScanPlayerScreen> createState() => _ScanPlayerScreenState();
+}
+
+class _ScanPlayerScreenState extends State<ScanPlayerScreen> {
+  bool get isUserFound => true;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return BackgroundWidget(
       child: SafeArea(
         child: Column(
           children: [
-            CustomChipWidget(title: "Scanning..."),
+            CustomChipWidget(title: isUserFound?"USER FOUND": "Scanning..."),
             Expanded(
               flex: 3,
               child: Center(
@@ -33,7 +45,11 @@ class ScanPlayerScreen extends StatelessWidget {
             CustomButton(
               title: "Start Game!",
               buttonStyleType: ButtonStyleType.green,
-              onTap: () {},
+              onTap: () {
+                if(isUserFound){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>TicTacToeScreen()));
+                }
+              },
             ),
             SizedBox(height: 50),
           ],
